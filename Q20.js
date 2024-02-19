475d0cc9fa1d90d23af75ea5dd31fc535f5d661f
Asnababar <147834277+Asnababar@users.noreply.github.com>
2024-02-19 18:57:59 +0500

    Node.js Q20

---
+++ Q20.js
@@ -0,0 +1,8 @@
+"use strict";
+// 20.Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+Object.defineProperty(exports, "__esModule", { value: true });
+const countries = ["Pakistan", "USA", "Canada", "Paris", "Turkey"];
+console.log("List of Countries:");
+for (let Country of countries) {
+    console.log(Country);
+}
---
+++ Q20.ts
@@ -0,0 +1,9 @@
+// 20.Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+const countries: string[] = ["Pakistan", "USA", "Canada", "Paris", "Turkey"]
+
+console.log("List of Countries:")
+
+for(let Country of countries){
+    console.log(Country)
+}
